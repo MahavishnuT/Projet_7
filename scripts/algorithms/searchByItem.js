61dda58e3222d03781929293898246b7453aad30
Tom Pujalte <tom.pujalte@gmail.com>
2023-01-03 11:48:03 +0100

    items search works fine

--- scripts/algorithms/searchByItem.js
+++ scripts/algorithms/searchByItem.js
@@ -1,28 +1,30 @@
 import { ingredientsNoRepeat, AppliancesNoRepeat, UstensilsNoRepeat, addItemsToDropdown } from "../pages/index.js";
 
 const searchIngredients = document.querySelector(".ingredients-input");
-const searchAppliance = document.querySelector(".ingredients-input");
-const searchUstensils = document.querySelector(".ingredients-input");
+const searchAppliance = document.querySelector(".appliance-input");
+const searchUstensils = document.querySelector(".ustensils-input");
 const ingredientsContainer = document.querySelector(".ingredients-container");
 const appliancesContainer = document.querySelector(".appliances-container");
 const ustensilsContainer = document.querySelector(".ustensils-container");
-console.log("ingredientsNoRepeat", ingredientsNoRepeat);
 
 
 
 searchIngredients.addEventListener("input", (event) => ((arg1, arg2) => {
   filterItems(event, arg1, arg2);
-  
 })(ingredientsNoRepeat, ingredientsContainer));
-// searchAppliance.addEventListener("input", filterItems(e, AppliancesNoRepeat, appliancesContainer));
-// searchUstensils.addEventListener("input", filterItems(e, UstensilsNoRepeat, ustensilsContainer));
+searchAppliance.addEventListener("input", (event) => ((arg1, arg2) => {
+  filterItems(event, arg1, arg2);
+})(AppliancesNoRepeat, appliancesContainer));
+searchUstensils.addEventListener("input", (event) => ((arg1, arg2) => {
+  filterItems(event, arg1, arg2);
+})(UstensilsNoRepeat, ustensilsContainer));
 
 function filterItems(event, arrayOfItems, containerOfItems) {
   const searchedString = event.target.value.toLowerCase();
   containerOfItems.innerHTML = "";
   console.log("arrayOfItems", arrayOfItems);
 
-  const result = arrayOfItems.forEach(item => {
+  const result = [...arrayOfItems].filter(item => {
     return (item.toLowerCase().includes(searchedString))
   })
   addItemsToDropdown(result, containerOfItems);
