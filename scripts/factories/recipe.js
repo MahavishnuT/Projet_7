64aed70c30dbd25438663cce6dbe38217531d85c
Tom Pujalte <tom.pujalte@gmail.com>
2023-01-09 22:08:30 +0100

    starting css

--- scripts/factories/recipe.js
+++ scripts/factories/recipe.js
@@ -1,50 +1,3 @@
-// function recipeFactory(data) {
-//   const { id, name, servings, ingredients, time, description, appliance, ustensils } = data;
-
-//   const recipeCardDOM = getRecipeCardDOM(name, time, description, ingredients);
-
-//   return {recipeCardDOM};
-// }
-
-// function getRecipeCardDOM(name, time, description, ingredients) {
-  
-//   const column = document.createElement("div");
-//   column.classList.add("col-12", "col-md-4");
-
-//   const card = document.createElement("div");
-//   card.classList.add("card");
-
-//   const img = document.createElement("img");
-//   img.setAttribute("style", "height: 180px");
-//   img.classList.add("card-img-top", "bg-light", "py-4");
-
-//   const cardBody = document.createElement("div");
-//   cardBody.classList.add("card-body");
-
-//   const cardTitle = document.createElement("h5");
-//   cardTitle.classList.add("card-title");
-//   cardTitle.textContent = name;
-
-//   const cardTime = document.createElement("span");
-//   cardTime.innerHTML = `<i class="bi bi-clock"></i> ${time} min`;
-
-//   const cardDescription = document.createElement("p");
-//   cardDescription.textContent = description;
-//   cardDescription.classList.add("card-text");
-
-//   const cardIngredients = document.createElement("div");
-//   const listIngredients = getIngredients(ingredients);
-//   for (let i = 0; i < listIngredients.length; i++) {
-//     cardIngredients.innerHTML += listIngredients[i]
-//   }
-
-//   cardBody.append(cardTitle, cardTime, cardIngredients, cardDescription);
-//   card.append(img, cardBody);
-//   column.appendChild(card);
-
-//   return(column);
-// }
-
 class RecipeCard {
   constructor(data) {
     this._id = data.id;
@@ -60,7 +13,7 @@ class RecipeCard {
   get recipeCard() {
 
     const card = `
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-lg-4">
         <div class="card">
           <img style="height: 180px" class="card-img-top bg-light py-4">
           <div class="card-body">
